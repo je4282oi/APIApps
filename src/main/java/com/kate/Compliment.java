a7acef4586d327f04f668aafd5df72ced1a36ff5
K <nonsense@gmail.com>
2018-04-12 18:17:44 -0500

    initial

---
+++ src/main/java/com/kate/Compliment.java
@@ -0,0 +1,13 @@
+package com.kate;
+
+
+class Compliment {
+    private String text;
+    Compliment (String text) {this.text = text;}
+
+    Compliment() {}
+
+    public String getText() { return text;}
+
+    public void setText(String text) {this.text = text;}
+}
---
+++ src/main/java/com/kate/HelloWorldApiApplication.java
@@ -0,0 +1,20 @@
+package com.kate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@Controller
+public class HelloWorldApiApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "index.html";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApiApplication.class, args);
+	}
+}
